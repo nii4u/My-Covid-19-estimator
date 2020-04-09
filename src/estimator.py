132c3f53224464bf4084d6ef433c8df4e3653a95
Lloyd Thompson <lthompson@aimsammi.org>
2020-04-09 21:00:08 +0200

    Estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,2 +1,21 @@
 def estimator(data):
-  return data
+  currentlyInfected = {}
+  currentlyInfected["data"] = data
+  dict1 = {}
+  dict1["currentlyInfected"] = data["reportedCases"] * 10
+  dict1["infectionsByRequestedTime"] = dict1["currentlyInfected"] * 512
+
+  currentlyInfected["impact"] = dict1
+  dict2 = {}
+  dict2["currentlyInfected"] = data["reportedCases"] * 50
+  dict2["infectionsByRequestedTime"] = dict2["currentlyInfected"] * 512
+  currentlyInfected["severeImpact"] = dict2
+
+  # currentlyInfected["impact"] = inputdata
+  # currentlyInfected = {
+  #   "data": inputdata,
+  #   "impact": {"currentlyInfected" : inputdata["reportedCases"] * 10 },
+  #   "severeImpact": {"currentlyInfected" : inputdata["reportedCases"] * 50 }
+  # }
+
+  return currentlyInfected
