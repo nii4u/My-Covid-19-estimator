1cfa0fafa9fb81238c2c45e76a795d9da8bc25c3
Lloyd Thompson <lthompson@aimsammi.org>
2020-04-09 23:40:59 +0200

    Building estimator

--- src/estimator.py
+++ src/estimator.py
@@ -1,15 +1,15 @@
 def estimator(data):
   currentlyInfected = {}
   currentlyInfected["data"] = data
-  dict1 = {}
-  dict1["currentlyInfected"] = data["reportedCases"] * 10
-  dict1["infectionsByRequestedTime"] = dict1["currentlyInfected"] * 512
+  currentlyInfected["impact"] = {}
+  currentlyInfected["impact"]["currentlyInfected"] = data["reportedCases"] * 10
+  currentlyInfected["impact"]["infectionsByRequestedTime"] = currentlyInfected["impact"]["currentlyInfected"] * 512
 
-  currentlyInfected["impact"] = dict1
-  dict2 = {}
-  dict2["currentlyInfected"] = data["reportedCases"] * 50
-  dict2["infectionsByRequestedTime"] = dict2["currentlyInfected"] * 512
-  currentlyInfected["severeImpact"] = dict2
+    
+  currentlyInfected["severeImpact"] = {}
+  currentlyInfected["severeImpact"]["currentlyInfected"] = data["reportedCases"] * 50
+  currentlyInfected["severeImpact"]["infectionsByRequestedTime"] = currentlyInfected["severeImpact"]["currentlyInfected"] * 512
+
 
   # currentlyInfected["impact"] = inputdata
   # currentlyInfected = {
