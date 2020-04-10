eaf861195c69e58b09fffb43153062ab7d69330d
Lloyd Thompson <lthompson@aimsammi.org>
2020-04-10 11:08:34 +0200

    Estimator

--- src/estimator.py
+++ src/estimator.py
@@ -3,19 +3,24 @@ def estimator(data):
   currentlyInfected["data"] = data
   currentlyInfected["impact"] = {}
   currentlyInfected["impact"]["currentlyInfected"] = data["reportedCases"] * 10
-  currentlyInfected["impact"]["infectionsByRequestedTime"] = currentlyInfected["impact"]["currentlyInfected"] * 512
 
-    
+  if data['periodType']=='days':
+      num_days = data['timeToElapse']
+  elif data['periodType']=='weeks':
+      num_days = data['timeToEplapse'] * 7
+  else:
+      num_days = data['timeToEplapse'] * 30
+
+  factor = num_days//3
+
+  multiplier = 2** factor
+
+  
+  currentlyInfected["impact"]["infectionsByRequestedTime"] = currentlyInfected["impact"]["currentlyInfected"] * multiplier
+
   currentlyInfected["severeImpact"] = {}
   currentlyInfected["severeImpact"]["currentlyInfected"] = data["reportedCases"] * 50
-  currentlyInfected["severeImpact"]["infectionsByRequestedTime"] = currentlyInfected["severeImpact"]["currentlyInfected"] * 512
+  currentlyInfected["severeImpact"]["infectionsByRequestedTime"] = currentlyInfected["severeImpact"]["currentlyInfected"] * multiplier
 
-
-  # currentlyInfected["impact"] = inputdata
-  # currentlyInfected = {
-  #   "data": inputdata,
-  #   "impact": {"currentlyInfected" : inputdata["reportedCases"] * 10 },
-  #   "severeImpact": {"currentlyInfected" : inputdata["reportedCases"] * 50 }
-  # }
 
   return currentlyInfected
